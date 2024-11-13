54bb4d9af0ca0cf9ada64224725b8623608a7f4e
Pedro Martins <pedromartinsdelemos@gmail.com>
2024-11-13 15:11:11 -0300

    DepartmentForm (dialog) design

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,12 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	public static Stage currentStage(ActionEvent event) {
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	} //receber o stage para onde o controle que recebeu o evento está
+}
