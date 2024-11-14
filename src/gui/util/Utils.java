1165bb69a3485f6a12f97fa445be85b75d7bff92
Pedro Martins <pedromartinsdelemos@gmail.com>
2024-11-14 10:27:33 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -9,4 +9,13 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	} //receber o stage para onde o controle que recebeu o evento está
+	
+	public static Integer tryParseToINt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
+
